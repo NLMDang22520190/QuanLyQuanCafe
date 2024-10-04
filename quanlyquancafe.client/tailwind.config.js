820a5fbb10884487861ccc05c2da11b123a32a98
Tekatoji <22520190@gm.uit.edu.vn>
2024-10-04 08:27:28 +0700

    Refactor Tailwind CSS configuration and add PostCSS configuration

---
+++ quanlyquancafe.client/tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+import fluid, { extract, screens } from 'fluid-tailwind'
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
+
+  content: {
+    files: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    extract
+  } ,
+  theme: {
+    screens,
+    extend: {
+      colors: {
+        primary: {
+          50: "#ffe8c7",
+          100 :"#f8cbab",
+          200 : "#deac8b",
+          300 :"#c28e68",
+          400: "#ad754d",
+          500: "#975e33",
+          600 :"#8b542d",
+          700 : "#7b4724",
+          800 : "#6d391d",
+          900 :"#5d2b14",
+
+        }
+      }
+
+    },
+    
+  },
+  plugins: [
+    fluid
+  ],
+});
+
