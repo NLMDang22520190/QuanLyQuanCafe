b3fffe4ded437de8e545c700a0571c9c46b581e4
Tekatoji <22520190@gm.uit.edu.vn>
2024-11-09 19:57:50 +0700

    Add Navbar component and update index.html title and favicon

--- quanlyquancafe.client/src/App.jsx
+++ quanlyquancafe.client/src/App.jsx
@@ -2,11 +2,12 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AllRoutes from "./AllRoutes";
+import Navbar from "./components/Users/Navbar/Navbar";
 
 function App() {
   <div className="App">
     <Router>
-      {/* <Navbar /> */}
+      <Navbar />
       <AllRoutes />
     </Router>
   </div>;
--- quanlyquancafe.client/src/components/Users/Navbar/Navbar.jsx
+++ quanlyquancafe.client/src/components/Users/Navbar/Navbar.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="bg-white shadow-md">
+      <div className="container mx-auto px-4 py-2 flex justify-between items-center">
+        <div className="text-2xl font-bold">THE COFFEE HOUSE</div>
+        <nav className="space-x-4">
+          <a href="#" className="text-gray-700">
+            Cà phê
+          </a>
+          <a href="#" className="text-gray-700">
+            Trà
+          </a>
+          <a href="#" className="text-gray-700">
+            Menu
+          </a>
+          <a href="#" className="text-gray-700">
+            Chuyện Nhà
+          </a>
+          <a href="#" className="text-gray-700">
+            Cảm hứng CloudFee
+          </a>
+          <a href="#" className="text-gray-700">
+            Cửa hàng
+          </a>
+          <a href="#" className="text-gray-700">
+            Tuyển dụng
+          </a>
+        </nav>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
