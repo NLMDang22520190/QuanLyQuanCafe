3281114febe2be3fafc02309d953f2664040a9e6
Vuvom1 <21522808@gm.uit.edu.vn>
2024-12-15 17:52:22 +0700

    Add Sale Report API

--- QuanLyQuanCafe.Server/Models/DTOs/MonthSalaryStatisticDTO.cs
+++ QuanLyQuanCafe.Server/Models/DTOs/MonthSalaryStatisticDTO.cs
@@ -6,5 +6,5 @@ public class MonthSalaryStatisticDTO
 {
     public string Month { get; set; }
     public int TotalHours { get; set; }
-    public decimal TotalSalaryPayed { get; set; }
+    public double TotalSalaryPayed { get; set; }
 }
